8ef55e1928381d7694e00d833fdccaa7e1dfd817
Samuel de Beer <samuelcdebeer@gmail.com>
2022-05-04 18:21:02 +0200

    Remove random test lines

--- BrowserDuel/Hubs/ChatHub.cs
+++ BrowserDuel/Hubs/ChatHub.cs
@@ -7,10 +7,13 @@ namespace BrowserDuel.Hubs
 {
     public class ChatHub : Hub
     {
+        int x => 2;
+        
         public async override Task OnConnectedAsync()
         {
             await base.OnConnectedAsync();
             Console.WriteLine("Connection");
+            var a = x;
         }
          
         public async Task SendMessage(string message)
